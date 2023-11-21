9a7068ca1c14a80d00b488d31dc523124fc979da
viingar <myuniverse8082@gmail.com>
2023-11-21 18:10:10 +0300

    Задание.5.4  Задание. Реализуйте кэширование данных на одну минуту, чтобы не обращаться при каждом запросе на сервер api.openweathermap.org.(Исправление удаления из кэша)

--- weathermicroservice/src/main/java/com/example/weathermicroservice/Configuration/AppConfiguration.java
+++ weathermicroservice/src/main/java/com/example/weathermicroservice/Configuration/AppConfiguration.java
@@ -4,8 +4,10 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
+@EnableScheduling
 @EnableCaching
 @Configuration
 public class AppConfiguration {
